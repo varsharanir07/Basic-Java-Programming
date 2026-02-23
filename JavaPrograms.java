0a4030fe9c8716cc41e08a09a2b48bc1af11693f
Varsha Rani <varsharani.r.07@gmail.com>
2026-02-23 11:32:51 +0530

    Level 3 done

--- JavaPrograms.java
+++ JavaPrograms.java
@@ -6,97 +6,91 @@ public class JavaPrograms {
 
         Scanner sc = new Scanner(System.in);
 
-        calculator(sc);
-        triangleArea(sc);
-        squareSide(sc);
-        distanceConversion(sc);
-        totalPurchase(sc);
-        quotientRemainder(sc);
+        celsiusToFahrenheit(sc);
+        fahrenheitToCelsius(sc);
+        totalIncome(sc);
+        travelDetails(sc);
 
         sc.close();
     }
 
-    // Basic Calculator
-    public static void calculator(Scanner sc) {
-        System.out.println("Enter two numbers:");
-        double number1 = sc.nextDouble();
-        double number2 = sc.nextDouble();
+    // 1️⃣ Celsius to Fahrenheit
+    public static void celsiusToFahrenheit(Scanner sc) {
+        System.out.println("Enter temperature in Celsius:");
+        double celsius = sc.nextDouble();
 
-        double add = number1 + number2;
-        double sub = number1 - number2;
-        double mul = number1 * number2;
-        double div = number2 != 0 ? number1 / number2 : 0;
+        double fahrenheitResult = (celsius * 9 / 5) + 32;
 
-        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
-                + number1 + " and " + number2 + " is "
-                + add + ", " + sub + ", " + mul + ", and " + div);
+        System.out.println("The " + celsius + " celsius is "
+                + fahrenheitResult + " fahrenheit");
         System.out.println();
     }
 
-    // Area of Triangle
-    public static void triangleArea(Scanner sc) {
-        System.out.println("Enter base and height in cm:");
-        double base = sc.nextDouble();
-        double height = sc.nextDouble();
+    // 2️⃣ Fahrenheit to Celsius
+    public static void fahrenheitToCelsius(Scanner sc) {
+        System.out.println("Enter temperature in Fahrenheit:");
+        double fahrenheit = sc.nextDouble();
 
-        double areaCm = 0.5 * base * height;
-        double areaInches = areaCm / (2.54 * 2.54);
+        double celsiusResult = (fahrenheit - 32) * 5 / 9;
 
-        System.out.println("The Area of the triangle in sq in is "
-                + areaInches + " and sq cm is " + areaCm);
+        System.out.println("The " + fahrenheit + " fahrenheit is "
+                + celsiusResult + " celsius");
         System.out.println();
     }
 
-    // Side of Square
-    public static void squareSide(Scanner sc) {
-        System.out.println("Enter perimeter of square:");
-        double perimeter = sc.nextDouble();
+    // 3️⃣ Total Income (Salary + Bonus)
+    public static void totalIncome(Scanner sc) {
+        System.out.println("Enter salary:");
+        double salary = sc.nextDouble();
 
-        double side = perimeter / 4;
+        System.out.println("Enter bonus:");
+        double bonus = sc.nextDouble();
 
-        System.out.println("The length of the side is "
-                + side + " whose perimeter is " + perimeter);
+        double income = salary + bonus;
+
+        System.out.println("The salary is INR " + salary +
+                " and bonus is INR " + bonus +
+                ". Hence Total Income is INR " + income);
         System.out.println();
     }
 
-    // Distance Conversion
-    public static void distanceConversion(Scanner sc) {
-        System.out.println("Enter distance in feet:");
-        double feet = sc.nextDouble();
+    // 4️⃣ Travel Details Program
+    public static void travelDetails(Scanner sc) {
 
-        double yards = feet / 3;
-        double miles = yards / 1760;
+        sc.nextLine();  // Clear buffer
 
-        System.out.println("The distance in yards is "
-                + yards + " while the distance in miles is " + miles);
-        System.out.println();
-    }
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
 
-    // Total Purchase Price
-    public static void totalPurchase(Scanner sc) {
-        System.out.println("Enter unit price and quantity:");
-        double unitPrice = sc.nextDouble();
-        int quantity = sc.nextInt();
+        System.out.println("Enter From City:");
+        String fromCity = sc.nextLine();
 
-        double totalPrice = unitPrice * quantity;
+        System.out.println("Enter Via City:");
+        String viaCity = sc.nextLine();
 
-        System.out.println("The total purchase price is INR "
-                + totalPrice + " if the quantity "
-                + quantity + " and unit price is INR " + unitPrice);
-        System.out.println();
-    }
+        System.out.println("Enter To City:");
+        String toCity = sc.nextLine();
 
-    // Quotient and Remainder
-    public static void quotientRemainder(Scanner sc) {
-        System.out.println("Enter two numbers:");
-        int number1 = sc.nextInt();
-        int number2 = sc.nextInt();
+        System.out.println("Enter distance from FromCity to ViaCity (in miles):");
+        double fromToVia = sc.nextDouble();
 
-        int quotient = number1 / number2;
-        int remainder = number1 % number2;
+        System.out.println("Enter distance from ViaCity to FinalCity (in miles):");
+        double viaToFinalCity = sc.nextDouble();
 
-        System.out.println("The Quotient is " + quotient +
-                " and Reminder is " + remainder +
-                " of two number " + number1 + " and " + number2);
+        System.out.println("Enter time from FromCity to ViaCity (in minutes):");
+        double timeFromToVia = sc.nextDouble();
+
+        System.out.println("Enter time from ViaCity to FinalCity (in minutes):");
+        double timeViaToFinalCity = sc.nextDouble();
+
+        double totalDistanceMiles = fromToVia + viaToFinalCity;
+        double totalDistanceKm = totalDistanceMiles * 1.60934; // miles to km
+        double totalTime = timeFromToVia + timeViaToFinalCity;
+
+        System.out.println("The Total Distance travelled by " + name +
+                " from " + fromCity + " to " + toCity +
+                " via " + viaCity + " is " +
+                totalDistanceKm + " km and the Total Time taken is "
+                + totalTime + " minutes");
     }
 }
