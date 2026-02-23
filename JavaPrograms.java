cb7017ba6a4a04996b3313cc13c2614d357e90f0
Varsha Rani <varsharani.r.07@gmail.com>
2026-02-23 11:27:17 +0530

    level 2 done

--- JavaPrograms.java
+++ JavaPrograms.java
@@ -1,88 +1,102 @@
+import java.util.Scanner;
+
 public class JavaPrograms {
 
     public static void main(String[] args) {
 
-        findAge();
-        distributePens();
-        calculateAveragePCM();
-        calculateProfitLoss();
-        calculateUniversityFee();
-        calculateEarthVolume();
+        Scanner sc = new Scanner(System.in);
+
+        calculator(sc);
+        triangleArea(sc);
+        squareSide(sc);
+        distanceConversion(sc);
+        totalPurchase(sc);
+        quotientRemainder(sc);
+
+        sc.close();
     }
 
-    // 1. Finding Age
-    public static void findAge() {
-        int birthYear = 2000;
-        int currentYear = 2024;
+    // Basic Calculator
+    public static void calculator(Scanner sc) {
+        System.out.println("Enter two numbers:");
+        double number1 = sc.nextDouble();
+        double number2 = sc.nextDouble();
 
-        int age = currentYear - birthYear;
+        double add = number1 + number2;
+        double sub = number1 - number2;
+        double mul = number1 * number2;
+        double div = number2 != 0 ? number1 / number2 : 0;
 
-        System.out.println("Harry's age in 2024 is " + age);
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is "
+                + add + ", " + sub + ", " + mul + ", and " + div);
         System.out.println();
     }
 
-    // 2. Pen Distribution
-    public static void distributePens() {
-        int totalPens = 14;
-        int totalStudents = 3;
+    // Area of Triangle
+    public static void triangleArea(Scanner sc) {
+        System.out.println("Enter base and height in cm:");
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
 
-        int pensPerStudent = totalPens / totalStudents;
-        int remainingPens = totalPens % totalStudents;
+        double areaCm = 0.5 * base * height;
+        double areaInches = areaCm / (2.54 * 2.54);
 
-        System.out.println("The Pen Per Student is " + pensPerStudent +
-                " and the remaining pen not distributed is " + remainingPens);
+        System.out.println("The Area of the triangle in sq in is "
+                + areaInches + " and sq cm is " + areaCm);
         System.out.println();
     }
 
-    // 3. Average PCM
-    public static void calculateAveragePCM() {
-        int maths = 94;
-        int physics = 95;
-        int chemistry = 96;
+    // Side of Square
+    public static void squareSide(Scanner sc) {
+        System.out.println("Enter perimeter of square:");
+        double perimeter = sc.nextDouble();
 
-        double average = (maths + physics + chemistry) / 3.0;
+        double side = perimeter / 4;
 
-        System.out.println("Sam’s average mark in PCM is " + average);
+        System.out.println("The length of the side is "
+                + side + " whose perimeter is " + perimeter);
         System.out.println();
     }
 
-    // 4. Profit & Loss
-    public static void calculateProfitLoss() {
-        double costPrice = 129;
-        double sellingPrice = 191;
+    // Distance Conversion
+    public static void distanceConversion(Scanner sc) {
+        System.out.println("Enter distance in feet:");
+        double feet = sc.nextDouble();
 
-        double profit = sellingPrice - costPrice;
-        double profitPercentage = (profit / costPrice) * 100;
+        double yards = feet / 3;
+        double miles = yards / 1760;
 
-        System.out.println("The Cost Price is INR " + costPrice +
-                " and Selling Price is INR " + sellingPrice +
-                "\nThe Profit is INR " + profit +
-                " and the Profit Percentage is " + profitPercentage);
+        System.out.println("The distance in yards is "
+                + yards + " while the distance in miles is " + miles);
         System.out.println();
     }
 
-    // 5. University Fee Discount
-    public static void calculateUniversityFee() {
-        double fee = 125000;
-        double discountPercent = 10;
+    // Total Purchase Price
+    public static void totalPurchase(Scanner sc) {
+        System.out.println("Enter unit price and quantity:");
+        double unitPrice = sc.nextDouble();
+        int quantity = sc.nextInt();
 
-        double discount = (fee * discountPercent) / 100;
-        double finalFee = fee - discount;
+        double totalPrice = unitPrice * quantity;
 
-        System.out.println("The discount amount is INR " + discount +
-                " and final discounted fee is INR " + finalFee);
+        System.out.println("The total purchase price is INR "
+                + totalPrice + " if the quantity "
+                + quantity + " and unit price is INR " + unitPrice);
         System.out.println();
     }
 
-    // 6. Earth Volume
-    public static void calculateEarthVolume() {
-        double radiusKm = 6378;
-        double volumeKm = (4.0 / 3.0) * Math.PI * Math.pow(radiusKm, 3);
+    // Quotient and Remainder
+    public static void quotientRemainder(Scanner sc) {
+        System.out.println("Enter two numbers:");
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
 
-        double volumeMiles = volumeKm * Math.pow(0.621371, 3);
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
 
-        System.out.println("The volume of earth in cubic kilometers is "
-                + volumeKm +
-                " and cubic miles is " + volumeMiles);
+        System.out.println("The Quotient is " + quotient +
+                " and Reminder is " + remainder +
+                " of two number " + number1 + " and " + number2);
     }
 }
