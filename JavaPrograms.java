d6078ccc696fb1d928029d3be3bb336b10e99144
Varsha Rani <varsharani.r.07@gmail.com>
2026-04-21 09:18:07 +0530

    Added Level 3 Java Programs

--- JavaPrograms.java
+++ JavaPrograms.java
@@ -6,221 +6,125 @@ public class JavaPrograms {
 
         Scanner sc = new Scanner(System.in);
 
-        factorsProgram(sc);
-        recursiveSum(sc);
-        leapYear(sc);
-        unitConverter(sc);
-        studentVote(sc);
-        friends(sc);
-        numberAnalysis(sc);
-        bmiProgram(sc);
-        quadratic(sc);
-        randomArray(sc);
+        footballHeights();
+        numberChecker(sc);
+        otpProgram();
+        collinear(sc);
+        euclidean(sc);
 
         sc.close();
     }
 
-    // 1️⃣ Factors Program
-    public static void factorsProgram(Scanner sc) {
+    // 1️⃣ Football Heights
+    public static void footballHeights() {
+        int[] heights = new int[11];
+
+        for (int i = 0; i < heights.length; i++) {
+            heights[i] = (int)(Math.random() * 101) + 150;
+            System.out.print(heights[i] + " ");
+        }
+
+        int sum = 0, min = heights[0], max = heights[0];
+
+        for (int h : heights) {
+            sum += h;
+            min = Math.min(min, h);
+            max = Math.max(max, h);
+        }
+
+        double mean = (double) sum / heights.length;
+
+        System.out.println("\nShortest = " + min);
+        System.out.println("Tallest = " + max);
+        System.out.println("Mean = " + mean);
+        System.out.println();
+    }
+
+    // 2️⃣ Number Checker
+    public static void numberChecker(Scanner sc) {
         System.out.println("Enter number:");
         int num = sc.nextInt();
 
-        int count = 0;
-        for (int i = 1; i <= num; i++)
-            if (num % i == 0) count++;
-
-        int[] factors = new int[count];
-        int index = 0;
-
-        for (int i = 1; i <= num; i++)
-            if (num % i == 0) factors[index++] = i;
-
-        int sum = 0, product = 1, squareSum = 0;
-
-        for (int f : factors) {
-            sum += f;
-            product *= f;
-            squareSum += Math.pow(f, 2);
+        int temp = num, count = 0;
+        while (temp != 0) {
+            count++;
+            temp /= 10;
         }
 
-        System.out.println("Factors:");
-        for (int f : factors) System.out.print(f + " ");
+        int[] digits = new int[count];
+        temp = num;
+        for (int i = 0; i < count; i++) {
+            digits[i] = temp % 10;
+            temp /= 10;
+        }
 
-        System.out.println("\nSum = " + sum);
-        System.out.println("Product = " + product);
-        System.out.println("Sum of Squares = " + squareSum);
-        System.out.println();
-    }
+        int sum = 0;
+        for (int d : digits) sum += d;
 
-    // 2️⃣ Recursive Sum
-    public static void recursiveSum(Scanner sc) {
-        System.out.println("Enter n:");
-        int n = sc.nextInt();
+        // Armstrong
+        int arm = 0;
+        for (int d : digits)
+            arm += Math.pow(d, count);
 
-        int rec = sumRec(n);
-        int formula = n * (n + 1) / 2;
+        System.out.println("Armstrong: " + (arm == num));
 
-        System.out.println("Recursive = " + rec);
-        System.out.println("Formula = " + formula);
-        System.out.println();
-    }
-
-    public static int sumRec(int n) {
-        if (n == 0) return 0;
-        return n + sumRec(n - 1);
-    }
-
-    // 3️⃣ Leap Year
-    public static void leapYear(Scanner sc) {
-        System.out.println("Enter year:");
-        int y = sc.nextInt();
-
-        if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
-            System.out.println("Leap Year");
-        else
-            System.out.println("Not Leap Year");
+        // Harshad
+        System.out.println("Harshad: " + (num % sum == 0));
 
         System.out.println();
     }
 
-    // 4️⃣ Unit Converter (basic)
-    public static void unitConverter(Scanner sc) {
-        System.out.println("Enter km:");
-        double km = sc.nextDouble();
-
-        double miles = km * 0.621371;
-
-        System.out.println("Miles = " + miles);
-        System.out.println();
-    }
-
-    // 5️⃣ Student Vote
-    public static void studentVote(Scanner sc) {
-        int[] ages = new int[10];
+    // 3️⃣ OTP Program
+    public static void otpProgram() {
+        int[] otp = new int[10];
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Enter age:");
-            ages[i] = sc.nextInt();
-
-            if (ages[i] >= 18)
-                System.out.println("Can Vote");
-            else
-                System.out.println("Cannot Vote");
+            otp[i] = (int)(Math.random() * 900000) + 100000;
+            System.out.print(otp[i] + " ");
         }
 
-        System.out.println();
-    }
+        boolean unique = true;
 
-    // 6️⃣ Friends (Youngest & Tallest)
-    public static void friends(Scanner sc) {
-        int[] age = new int[3];
-        int[] height = new int[3];
-
-        System.out.println("Enter ages:");
-        for (int i = 0; i < 3; i++) age[i] = sc.nextInt();
-
-        System.out.println("Enter heights:");
-        for (int i = 0; i < 3; i++) height[i] = sc.nextInt();
-
-        int minAge = age[0], maxHeight = height[0];
-
-        for (int i = 1; i < 3; i++) {
-            if (age[i] < minAge) minAge = age[i];
-            if (height[i] > maxHeight) maxHeight = height[i];
-        }
-
-        System.out.println("Youngest age = " + minAge);
-        System.out.println("Tallest height = " + maxHeight);
-        System.out.println();
-    }
-
-    // 7️⃣ Number Analysis
-    public static void numberAnalysis(Scanner sc) {
-        int[] arr = new int[5];
-
-        for (int i = 0; i < 5; i++) {
-            arr[i] = sc.nextInt();
-
-            if (arr[i] >= 0) {
-                if (arr[i] % 2 == 0)
-                    System.out.println("Positive Even");
-                else
-                    System.out.println("Positive Odd");
-            } else {
-                System.out.println("Negative");
+        for (int i = 0; i < otp.length; i++) {
+            for (int j = i + 1; j < otp.length; j++) {
+                if (otp[i] == otp[j]) unique = false;
             }
         }
 
-        if (arr[0] > arr[4]) System.out.println("First greater");
-        else if (arr[0] < arr[4]) System.out.println("Last greater");
-        else System.out.println("Equal");
+        System.out.println("\nAll OTPs unique: " + unique);
+        System.out.println();
+    }
+
+    // 4️⃣ Collinear Points
+    public static void collinear(Scanner sc) {
+        System.out.println("Enter x1 y1 x2 y2 x3 y3:");
+        double x1 = sc.nextDouble(), y1 = sc.nextDouble();
+        double x2 = sc.nextDouble(), y2 = sc.nextDouble();
+        double x3 = sc.nextDouble(), y3 = sc.nextDouble();
+
+        double area = 0.5 * (x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+
+        if (area == 0)
+            System.out.println("Collinear");
+        else
+            System.out.println("Not Collinear");
 
         System.out.println();
     }
 
-    // 8️⃣ BMI Program
-    public static void bmiProgram(Scanner sc) {
-        double[][] data = new double[10][3];
+    // 5️⃣ Euclidean Distance & Line
+    public static void euclidean(Scanner sc) {
+        System.out.println("Enter x1 y1 x2 y2:");
+        double x1 = sc.nextDouble(), y1 = sc.nextDouble();
+        double x2 = sc.nextDouble(), y2 = sc.nextDouble();
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Enter weight(kg) and height(cm):");
-            data[i][0] = sc.nextDouble();
-            data[i][1] = sc.nextDouble();
+        double distance = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
 
-            double h = data[i][1] / 100;
-            data[i][2] = data[i][0] / (h * h);
+        double m = (y2 - y1) / (x2 - x1);
+        double b = y1 - m * x1;
 
-            System.out.println("BMI = " + data[i][2]);
-        }
-
-        System.out.println();
-    }
-
-    // 9️⃣ Quadratic Equation
-    public static void quadratic(Scanner sc) {
-        System.out.println("Enter a b c:");
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        double c = sc.nextDouble();
-
-        double d = b * b - 4 * a * c;
-
-        if (d > 0) {
-            double r1 = (-b + Math.sqrt(d)) / (2 * a);
-            double r2 = (-b - Math.sqrt(d)) / (2 * a);
-            System.out.println("Roots: " + r1 + ", " + r2);
-        } else if (d == 0) {
-            double r = -b / (2 * a);
-            System.out.println("Root: " + r);
-        } else {
-            System.out.println("No real roots");
-        }
-
-        System.out.println();
-    }
-
-    // 🔟 Random Array
-    public static void randomArray(Scanner sc) {
-        int[] arr = new int[5];
-
-        for (int i = 0; i < 5; i++) {
-            arr[i] = (int)(Math.random() * 9000) + 1000;
-            System.out.print(arr[i] + " ");
-        }
-
-        int min = arr[0], max = arr[0], sum = 0;
-
-        for (int x : arr) {
-            sum += x;
-            min = Math.min(min, x);
-            max = Math.max(max, x);
-        }
-
-        double avg = (double) sum / arr.length;
-
-        System.out.println("\nAvg = " + avg);
-        System.out.println("Min = " + min);
-        System.out.println("Max = " + max);
+        System.out.println("Distance = " + distance);
+        System.out.println("Line: y = " + m + "x + " + b);
         System.out.println();
     }
 }
